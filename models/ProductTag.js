51d3a1a6088aec46d17f5c23d2a68003fbecc3a5
sdavi222 <130076128+sdavi222@users.noreply.github.com>
2023-10-01 08:30:25 -0400

    Added models folder

---
+++ models/ProductTag.js
@@ -0,0 +1,44 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
+
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
+class ProductTag extends Model { }
+
+// set up fields and rules for ProductTag model
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: "id"
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: "id"
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
